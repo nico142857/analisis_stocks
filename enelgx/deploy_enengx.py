256f77e3c26815da7ab9db071c557ab5d6b0d8cb
nico142857 <nicolas.toro.llanco@gmail.com>
2025-03-10 20:58:00 +0100

    up

--- enelgx/deploy_enengx.py
+++ enelgx/deploy_enengx.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 @st.cache_data
 def load_data():
@@ -11,10 +11,42 @@ def load_data():
 
 df = load_data()
 
-st.title("Informacion EnelGXCH")
+st.title("Informacion Enel Generacion Chile")
 
 metrics = st.multiselect("Selecciona métricas:", df.index, default=[df.index[0]])
 
-fig = px.line(df.T, y=metrics, title=f"Evolución de {', '.join(metrics)}", markers=True)
+fig = go.Figure()
+
+if len(metrics) > 0:
+    fig.add_trace(go.Scatter(
+        x=df.T.index, y=df.T[metrics[0]],
+        mode="lines+markers",
+        name=metrics[0],
+        yaxis="y1"
+    ))
+
+if len(metrics) > 1:
+    fig.add_trace(go.Scatter(
+        x=df.T.index, y=df.T[metrics[1]],
+        mode="lines+markers",
+        name=metrics[1],
+        yaxis="y2"
+    ))
+    fig.update_layout(
+        yaxis2=dict(
+            title=metrics[1],
+            overlaying="y",
+            side="right",
+            showgrid=False
+        )
+    )
+
+fig.update_layout(
+    title=f"Evolución de {', '.join(metrics)}",
+    xaxis_title="Trimestres",
+    yaxis=dict(title=metrics[0]),
+    legend=dict(x=0, y=1),
+    xaxis_tickangle=270
+)
 
 st.plotly_chart(fig, use_container_width=True)
